90d9c3da6d0a614960bf7b42ad0c734625da7bda
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-11-08 13:57:04 +0300

    update codd

--- app.js
+++ app.js
@@ -10,32 +10,22 @@ app.get("/fetch-clickup", async (req, res) => {
   try {
     // Make the POST request to the ClickUp API
     const response = await fetch(
-      "https://app.clickup.com/api?client_id=WMJI5VECN2FG0EETI5D1R1DOZJOX139H&redirect_uri=https://loquacious-cassata-078d95.netlify.app/%23/",
+      "https://api.clickup.com/api/v2/oauth/token?client_id=WMJI5VECN2FG0EETI5D1R1DOZJOX139H&client_secret=4CKQU8TJVJEU3HH8RT7LIO3ADWIX7UB9ERARYSM7CWZX2VBBGV9HFW1R76A49CRK&code=S117Q6VYK9SAZGQFE8E5I7XDIYL0QF3X",
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(req.body), // Ensure you're sending JSON as the body if needed
       }
     );
+
+    console.log(response);
 
     // Check if the response is OK (status 200)
     if (!response.ok) {
       throw new Error(`Request failed with status: ${response.status}`);
     }
 
-    // If the response is HTML, send it as raw HTML to the browser
-    const contentType = response.headers.get("content-type");
-    if (contentType && contentType.includes("text/html")) {
-      const htmlContent = await response.text();
-      return res.send(htmlContent); // Send HTML response as raw HTML
-    }
-
-    // Parse response data if it's JSON
-    const data = await response.json();
-
-    // Send JSON response back to the client
     res.status(200).json(data);
   } catch (error) {
     console.error("Error:", error);
